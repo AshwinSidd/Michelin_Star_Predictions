1bd6afe324b687c83d555d15ada11f2b841cefb7
Juliana dvdS <julianadvds@gmail.com>
2022-04-01 19:32:53 -0400

    completed database connection

---
+++ create_database.sql
@@ -0,0 +1,27 @@
+	CREATE TABLE 	restaurant_data (
+		index INT PRIMARY KEY NOT NULL,
+		restaurant_name VARCHAR,
+		country VARCHAR NOT NULL,
+		latitude  REAL,
+		longitude REAL,
+		awards VARCHAR, 
+		price_level INT,
+		avg_rating REAL,
+		total_reviews_count INT,
+		michelin INT,
+		any_award INT
+	);
+		
+-- DROP TABLE restaurant_data
+
+	CREATE TABLE country_data (
+		index INT PRIMARY KEY NOT NULL,
+		country VARCHAR,
+		population INT,
+		area INT,
+		pop_density REAL,
+		gdp REAL,
+		service INT
+	)
+	
+-- DROP TABLE country_data
